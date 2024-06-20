7b658ece9bfb3224dbaecaa8473307723bfc0a58
LucasBatista11 <165969437+LucasBatista11@users.noreply.github.com>
2024-06-20 14:43:12 -0300

    Primeiro Commit
    Commit 01

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleClass() {
+    var element = document.getElementById("offCanvas-wrap");
+    element.classList.toggle("offCanvas-wrap");
+    element.classList.toggle("offCanvas-wrap-view");
+}
