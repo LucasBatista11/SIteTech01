df0aad3b8486abb56e5655c72f8cd047816f74a4
LucasBatista11 <165969437+LucasBatista11@users.noreply.github.com>
2024-06-21 10:26:49 -0300

    Commit 2
    Commit 2
    - Loader Home;

--- pageLoader.js
+++ pageLoader.js
@@ -1,27 +1,28 @@
-window.addEventListener("load", function() {
-    const loader = document.getElementById("loader");
-    setTimeout(function() {
-      loader.style.opacity = "0";
-      setTimeout(function() {
-        loader.style.display = "none";
-      }, 2000);
-    }, 2000);
-  });
 
-  lottie.loadAnimation({
-    container: document.getElementById("lottie-loader"),
-    renderer: "svg",
-    loop: false,
-    autoplay: true,
-    path: "imagens/pageLoader.js",
-    rendererSettings: {
-      progressiveLoad: false
-    },
-    animationData: null,
-    renderer: 'canvas',
-    images: null,
-    events: {
-      complete: function() {console.log('Animation Loaded')},
-      error: function(e) {console.error(e)}
-    }
-  });
+window.addEventListener("load", function() {
+  const loader = document.getElementById("loader");
+  setTimeout(function() {
+    loader.style.opacity = "0";
+    setTimeout(function() {
+      loader.style.display = "none";
+    }, 3000);
+  }, 3000);
+});
+
+lottie.loadAnimation({
+  container: document.getElementById("lottie-loader"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "imagens/pageloader.json",
+  rendererSettings: {
+    progressiveLoad: false
+  },
+  animationData: null,
+  renderer: 'canvas',
+  images: null,
+  events: {
+    complete: function() {console.log('Animation Loaded')},
+    error: function(e) {console.error(e)}
+  }
+});
